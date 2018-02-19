e4f2a986f1af7cd1a71bd1af5b79d30afb117e4b
dantogno <vebulidbih@msn.com>
2018-02-19 10:11:17 -0600

    initial project add

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // public delegate void CoinCountValueChangedDelegate(int newCoinCount);
+
+    // Using Action<T> allows us to skip defining our own delegate template
+    public static event Action<int> CoinCountValueChanged;
+
+    private static int coinCountUseProperty;
+
+    public static int CoinCount
+    {
+        get { return coinCountUseProperty; }
+        private set
+        {
+            coinCountUseProperty = value;
+            if (CoinCountValueChanged != null)
+                CoinCountValueChanged.Invoke(coinCountUseProperty);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        CollectCoin();
+    }
+
+    private void CollectCoin()
+    {
+        CoinCount++;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/HeadingText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadingText : MonoBehaviour
+{
+    [SerializeField]
+    private int coinsRequiredToWin = 3;
+
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void OnCoinValueChanged(int coinCount)
+    {
+        UpdateText(coinCount);
+    }
+
+    private void UpdateText(int coinCount)
+    {
+        if (CheckForWin(coinCount))
+        {
+            text.text = "You win!";
+        }
+        else
+        {
+            int coinsNeeded = coinsRequiredToWin - coinCount;
+            text.text = String.Format("You need {0} more coin(s)!", coinsNeeded);
+        }
+    }
+
+    private bool CheckForWin(int coinCount)
+    {
+        bool playerHasWon = coinCount == coinsRequiredToWin;
+
+        return playerHasWon;
+    }
+
+    // Subscribe and unsubscribe from the event
+    private void OnEnable()
+    {
+        Coin.CoinCountValueChanged += OnCoinValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        Coin.CoinCountValueChanged -= OnCoinValueChanged;
+    }
+}
